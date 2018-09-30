0d31b1a17a4223af2db74894b7738bb26f887d1a
nekocode <nekocode.cn@gmail.com>
2018-09-30 12:32:27 +0800

    Add post model

--- src/resolvers/Query.ts
+++ src/resolvers/Query.ts
@@ -1,9 +1,11 @@
 import { Context } from '..'
-import { getUserId } from '../utils'
+import { getUserId, forwardTo } from '../utils'
 
 export const Query = {
   me: async (root, args, ctx: Context) => {
     const user = await ctx.db.user({ id: getUserId(ctx) });
     return user;
   },
+
+  posts: forwardTo('db'),
 }
